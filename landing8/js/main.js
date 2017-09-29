08bd2e0a29a6bc3d33f20991195fac8a9b483bc9
Matthew Speiser <mattrspeiser@gmail.com>
2017-09-28 23:16:00 -0700

    added underline effect and fixed bug for nav

--- landing8/js/main.js
+++ landing8/js/main.js
@@ -4,6 +4,8 @@
   	$('aside').toggleClass('left-25pct');
   	$('.togg').toggleClass('left25pct');
   	$('.fa-chevron-right').toggleClass('rotate180 left25pct');
+  	$('.wrapper').toggleClass('left25pct');
+  	$('.underline-fx').toggleClass('underline');
   });
 
 })();
