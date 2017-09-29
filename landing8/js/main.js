cb7336075de9fe5afb3a4d66e86ee02eae0eaba1
Matthew Speiser <mattrspeiser@gmail.com>
2017-09-28 21:05:16 -0700

    new landing page with responsive navbar pushing wrapped content with some animation transitions

---
+++ landing8/js/main.js
@@ -0,0 +1,9 @@
+(function(){
+
+  $('.togg').click(function(){
+  	$('aside').toggleClass('left-25pct');
+  	$('.togg').toggleClass('left25pct');
+  	$('.fa-chevron-right').toggleClass('rotate180 left25pct');
+  });
+
+})();
