0957291714d65dce07c1508972b1a794cd7a113e
Matthew Speiser <mattrspeiser@gmail.com>
2017-09-30 15:35:19 -0700

    added 2 grids with responsive aside bar and footer

--- landing8/js/main.js
+++ landing8/js/main.js
@@ -2,12 +2,17 @@
 
   $('.title').removeClass('top-100px');
 
+  setTimeout(function(){
+  	$('.underline-fx').addClass('underline');
+  }, 1000);
+  	
   $('.togg').click(function(){
   	$('aside').toggleClass('left-25pct');
   	$('.togg').toggleClass('left25pct');
   	$('.fa-chevron-right').toggleClass('rotate180 left25pct');
   	$('.wrapper').toggleClass('left25pct');
   	$('.underline-fx').toggleClass('underline');
+  	$('.title').toggleClass('top-100px');
   });
 
 })();
